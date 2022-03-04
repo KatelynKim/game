ac88ab1e9bb562d722f46f38567f780729723677
Katelyn Kim <wlsdud194@hotmail.com>
2022-03-03 22:33:55 -0800

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+ 
+import io from "socket.io-client";
+import { useState } from "react"; 
+
+const socket = io.connect("http://localhost:3001");
+
+function App() {
+   
+
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
